9ac9a761667ba7abc7b6c092f2bee0176bb47e23
nickubed <nhpquandt@gmail.com>
2021-05-26 11:22:28 -0700

    cleaned

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,6 +1,5 @@
 import {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import {useParams, Link} from 'react-router-dom'
 
 const ArtistView = (props) => {
     const { id } = useParams()
